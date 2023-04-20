dc3eaf777e275f5b73182ec7609ae564a6f34d25
KarimAraybi <karim.araybi@lau.edu>
2023-04-20 23:37:44 +0300

    Added iFrames

--- Unnamed game/Assets/Scripts/Health/Health.cs
+++ Unnamed game/Assets/Scripts/Health/Health.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    [Header("Header")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
  
     }
     public void TakeDamage(float _damage)
@@ -21,6 +30,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -34,15 +44,27 @@ public class Health : MonoBehaviour
     }
     
     
-    public void Update()
-    {
+    // public void Update()
+    // {
         
-        if(Input.GetKeyDown(KeyCode.E))
-        TakeDamage(1);
-    }
+    //     if(Input.GetKeyDown(KeyCode.E))
+    //          TakeDamage(1);
+    // }
 
     public void AddHealth(float _value){
         currentHealth = Mathf.Clamp(currentHealth + _value,0,startingHealth);
+    }
+
+    private IEnumerator Invulnerability(){
+        Physics2D.IgnoreLayerCollision(11,12,true);
+        for(int i = 0; i<numberOfFlashes;i++){
+            spriteRend.color = new Color(1,0,0,0.5f);
+            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+        }
+        Physics2D.IgnoreLayerCollision(11,12,false);
+
     }
 
 }
