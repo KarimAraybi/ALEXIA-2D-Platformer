2bcc95a7d91e65d7b6b24bd1b8bec57120fd7963
OmarZaatari <106358964+OmarZaatari@users.noreply.github.com>
2023-04-25 17:40:00 +0300

    Enemy
    Added enemy and player prefabs with updates to the health script

--- Unnamed game/Assets/Scripts/Health/Health.cs
+++ Unnamed game/Assets/Scripts/Health/Health.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Health : MonoBehaviour
 {
-    [Header("Header")]
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
@@ -11,60 +11,67 @@ public class Health : MonoBehaviour
 
     [Header("iFrames")]
     [SerializeField] private float iFramesDuration;
-    [SerializeField] private float numberOfFlashes;
+    [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
 
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
 
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
         spriteRend = GetComponent<SpriteRenderer>();
- 
     }
     public void TakeDamage(float _damage)
     {
+        if (invulnerable) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-        
 
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            StartCoroutine(Invulnerability());
+            StartCoroutine(Invunerability());
         }
         else
         {
             if (!dead)
-                {
-                    anim.SetTrigger("die");
-                    GetComponent<PlayerMovement>().enabled = false;
-                    dead = true;
-                }
+            {
+                anim.SetTrigger("die");
+
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
+
+                dead = true;
+            }
         }
     }
-    
-    
-    // public void Update()
-    // {
-        
-    //     if(Input.GetKeyDown(KeyCode.E))
-    //          TakeDamage(1);
-    // }
-
-    public void AddHealth(float _value){
-        currentHealth = Mathf.Clamp(currentHealth + _value,0,startingHealth);
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
-
-    private IEnumerator Invulnerability(){
-        Physics2D.IgnoreLayerCollision(11,12,true);
-        for(int i = 0; i<numberOfFlashes;i++){
-            spriteRend.color = new Color(1,0,0,0.5f);
-            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+    private IEnumerator Invunerability()
+    {
+        invulnerable = true;
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
             spriteRend.color = Color.white;
-            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
-        Physics2D.IgnoreLayerCollision(11,12,false);
-
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
     }
 
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+        this.transform.parent.gameObject.SetActive(false);
+    }
 }
+
+    
